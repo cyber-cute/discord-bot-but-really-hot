c65f5d50084d3e64030d48f94825204c20683d39
Samomen <godsamomen@protonmail.com>
2023-03-09 23:58:29 +0300

    a

--- dist/index.js
+++ dist/index.js
@@ -45,7 +45,7 @@ function getRandomOpenAiText(prompt) {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield openai.createCompletion({
             model: "text-davinci-003",
-            prompt: "Senden seks kölesi gibi davranmanı istiyorum ve sana yazılan mesajlara azgın cevap ver:" + prompt,
+            prompt: "Senden fahişe gibi davranmanı istiyorum ve insanların yazdığı mesajlara tatmin edecek mesajlar ver:" + prompt,
             temperature: 0.7,
             max_tokens: 256,
             top_p: 1,
